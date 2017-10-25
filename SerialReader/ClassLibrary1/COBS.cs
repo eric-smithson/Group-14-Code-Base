400ba243c567904a3651e7c8f1c4810c1579f15c
danebouchie <danebouchie@knights.ucf.edu>
2017-10-25 19:38:24 -0400

    Added serial interface
    Added serial inteface and PiSerial

--- SerialReader/ClassLibrary1/COBS.cs
+++ SerialReader/ClassLibrary1/COBS.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Consistent_Overhead_Byte_Stuffing {
 	/// <summary>
---
+++ SerialReader/ClassLibrary1/PiSerial.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class PiSerial : ISerial
+    {
+        SerialPort com;
+
+        public PiSerial()
+        {
+            OpenCom();
+        }
+
+        private void OpenCom()
+        {
+            Debug.WriteLine("Setting up serial reader");
+
+            com = new SerialPort();
+            com.PortName = "COM7";
+            com.BaudRate = 115200;
+            com.DataBits = 8;
+            com.Parity = Parity.None;
+            com.StopBits = StopBits.One;
+
+            // virtual config
+            // com.PortName = "COM4";
+            // com.BaudRate = 115200;
+            // com.DataBits = 8;
+            // com.Parity = Parity.None;
+            // com.StopBits = StopBits.One;
+
+            com.Open();
+
+            Debug.WriteLine(com);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            return com.Read(buffer, offset, count);
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            com.Write(buffer, offset, count);
+        }
+    }
+}

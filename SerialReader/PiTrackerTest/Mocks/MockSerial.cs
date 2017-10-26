037c8a598750ecc8fd5668f64a8e998e497412c4
danebouchie <danebouchie@knights.ucf.edu>
2017-10-25 20:03:08 -0400

    Added MockSerial
    Added MockSerial for unit testing.

---
+++ SerialReader/PiTrackerTest/Mocks/MockSerial.cs
@@ -0,0 +1,55 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiTrackerTest.Mocks
+{
+    class MockSerial : ISerial
+    {
+        public byte[] readByteBuffer = new byte[0];
+
+        public class BytesWrittenEventArgs : EventArgs
+        {
+            public byte[] Bytes;
+            public BytesWrittenEventArgs(byte[] bytes)
+            {
+                Bytes = (byte[]) bytes.Clone();
+            }
+        }
+
+        public event EventHandler<BytesWrittenEventArgs> OnBytesWritten;
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            // Make sure we are not requesting more than the current buffer can supply
+            int bytesToRead = Math.Min(readByteBuffer.Length, count);
+
+            // Copy data to buffer
+            buffer = new byte[bytesToRead];
+            readByteBuffer.CopyTo(buffer, 0);
+
+            return bytesToRead;
+        }
+
+        public void Write(byte[] buffer, int offset, int count)
+        {
+            // Copy written bytes to eventargs
+            BytesWrittenEventArgs written = new BytesWrittenEventArgs(
+                buffer.Skip(offset).Take(count).ToArray());
+
+            OnBytesWritten?.Invoke(this, written);
+        }
+
+        public void GiveReadBytes(byte[] bytesToRead)
+        {
+            // Resize readByteBuffer and append new data
+            byte[] temp = new byte[bytesToRead.Length + readByteBuffer.Length];
+            readByteBuffer.CopyTo(temp, 0);
+            bytesToRead.CopyTo(temp, readByteBuffer.Length);
+            readByteBuffer = temp;
+        }
+    }
+}

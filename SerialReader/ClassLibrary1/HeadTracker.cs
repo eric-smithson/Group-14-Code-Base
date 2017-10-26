f1d54028ac8275c4914c7b26b08a6ec7a75a7f3e
danebouchie <danebouchie@knights.ucf.edu>
2017-10-25 21:46:04 -0400

    Updated DLL Front End

--- SerialReader/ClassLibrary1/HeadTracker.cs
+++ SerialReader/ClassLibrary1/HeadTracker.cs
@@ -2,19 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace ClassLibrary1
 {
     public class HeadTracker
     {
-        public event EventHandler<PositionDataEventArgs> PositionDataUpdate;
-        void UpdatedPosition(float x, float y, float z)
+        public static HeadTracker Singleton;
+
+        public class PositionDataEventArgs : EventArgs
         {
-            PositionDataEventArgs pdea = new PositionDataEventArgs(x, y, z);
-            PositionDataUpdate?.BeginInvoke(this, pdea, null, null);
+            public Vector3 LeftEye;
+            public Vector3 RightEye;
+            public Boolean LostTracking
+            {
+                get
+                {
+                    return float.IsNaN(LeftEye.x)
+                        || float.IsNaN(LeftEye.y)
+                        || float.IsNaN(LeftEye.z)
+                        || float.IsNaN(RightEye.x)
+                        || float.IsNaN(RightEye.y)
+                        || float.IsNaN(RightEye.z);
+                }
+            }
+
+            public PositionDataEventArgs(float le_x, float le_y, float le_z,
+                float re_x, float re_y, float re_z)
+            {
+                LeftEye = new Vector3(le_x, le_y, le_z);
+                RightEye = new Vector3(re_x, re_y, re_z);
+            }
         }
 
-        public void StartDistortionCalibration()
+        public class OutputEventArgs : EventArgs
+        {
+            public String Output;
+
+            public OutputEventArgs(string output)
+            {
+                Output = output;
+            }
+        }
+
+        public event EventHandler<PositionDataEventArgs> PositionDataUpdate;
+        public event EventHandler<OutputEventArgs> Output;
+
+        public void StartDistortionCalibration(int cameraNumber)
+        {
+
+        }
+
+        public void AddCalibrationPoint(Vector3 point, float[] quater)
         {
 
         }
@@ -24,23 +63,22 @@ namespace ClassLibrary1
 
         }
 
-        public int FavoriteNumber()
+        public long FavoriteNumber()
         {
-            return 666;
+            return 0xDEADBEEF;
         }
 
-        // public void AddCalibrationPoint(Vector3 point)
-    }
-
-    public class PositionDataEventArgs : EventArgs
-    {
-        public float X, Y, Z;
-        public PositionDataEventArgs(float x, float y, float z)
+        public void SetEyeDistance(float dis)
         {
 
-            this.X = x;
-            this.Y = y;
-            this.Z = z;
+        }
+
+        void UpdatePosition(float le_x, float le_y, float le_z,
+        float re_x, float re_y, float re_z)
+        {
+            PositionDataEventArgs pdArgs = new PositionDataEventArgs(le_x, le_y,
+                le_z, re_x, re_y, re_z);
+            PositionDataUpdate?.BeginInvoke(this, pdArgs, null, null);
         }
     }
 }
